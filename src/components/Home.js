2890ebd4d4d4b570631ef760a051f7951b9e5d4b
Bohdan Fedirko <bohhhhdan@gmail.com>
2023-05-09 14:29:17 +0200

    Particle, button second component and animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
 import React from "react";
 import classes from "./Home.module.css";
+import LinksAndButton from "./UI/LinksAndButton";
+import Particle from "./UI/Particle.tsx";
 
 const Home = () => {
+
   return (
     <div className={classes.container}>
-      <div >
+      <Particle />
+      <div className={classes.textcont}>
         <h2>Hi, I'm Bohdan Fedirko</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco l
         </p>
+        <LinksAndButton link={"#second"} alt={"Lets start"} >Lets start</LinksAndButton>
       </div>
     </div>
   );
